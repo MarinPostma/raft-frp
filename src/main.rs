90c1ba1b79e8ceb477fcf86b0ef4d58158098559
mpostma <postma.marin@protonmail.com>
2020-08-05 13:55:23 +0200

    clean interface to raft

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,13 @@ mod raft_node;
 mod message;
 mod raft;
 
-use std::sync::{RwLock, Arc};
+use std::sync::Arc;
 use std::collections::HashMap;
 use structopt::StructOpt;
 use actix_web::{get, web, App, HttpServer, Responder};
-use tokio::sync::mpsc;
-use tokio::sync::oneshot;
-use crate::message::Message;
-use crate::raft::MyMessage;
+use crate::raft::{Raft, Mailbox, Store, RaftError};
+use serde::{Serialize, Deserialize};
+use log::info;
 
 #[derive(Debug, StructOpt)]
 struct Options {
@@ -23,16 +22,30 @@ struct Options {
     web_server: Option<String>,
 }
 
-#[get("/put/{id}/{name}")]
-async fn put(sender: web::Data<mpsc::Sender<Message<MyMessage>>>, path: web::Path<(u64, String)>) -> impl Responder {
-    let mut sender = sender.into_inner().as_ref().clone();
-    let (tx, rx) = oneshot::channel();
-    let _ = sender.send(Message::Propose {
-        proposal: MyMessage::Insert { key: path.0, value: path.1.clone() },
-        chan: tx,
+#[derive(Serialize, Deserialize)]
+pub enum Message {
+    Insert { key: u64, value: String }
+}
 
-    }).await;
-    let _ = rx.await;
+impl Store for HashMap<u64, String> {
+    type Message = Message;
+    type Error = RaftError;
+
+    fn apply(&mut self, message: Self::Message) -> Result<(), Self::Error> {
+        match message {
+            Message::Insert { key, value } => {
+                info!("inserting: ({}, {})", key, value);
+                self.insert(key, value);
+            }
+        }
+        Ok(())
+    }
+}
+
+#[get("/put/{id}/{name}")]
+async fn put(mailbox: web::Data<Mailbox<Message>>, path: web::Path<(u64, String)>) -> impl Responder {
+    let message = Message::Insert { key: path.0, value: path.1.clone() };
+    mailbox.send(message).await.unwrap();
     "OK".to_string()
 }
 
@@ -40,191 +53,38 @@ async fn put(sender: web::Data<mpsc::Sender<Message<MyMessage>>>, path: web::Pat
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     env_logger::init();
     let options = Options::from_args();
-    let store = Arc::new(RwLock::new(HashMap::new()));
+    let store = HashMap::new();
 
     // setup runtime for actix
     let local = tokio::task::LocalSet::new();
     let sys = actix_rt::System::run_in_tokio("server", &local);
 
-    let (tx, rx) = mpsc::channel(100);
     match options.peer_addr {
         Some(addr) => {
-            let _raft = raft::join(&options.raft_addr, &addr, store.clone(), tx, rx).await?;
+            Raft::new(options.raft_addr, store)
+                .join(&addr)
+                .await?;
         }
         None => {
-
-            let raft = tokio::spawn(raft::run(options.raft_addr, store.clone(), tx.clone(), rx));
-            let server = tokio::spawn(
+            let raft = Raft::new(options.raft_addr, store);
+            let mailbox = Arc::new(raft.mailbox());
+            let raft = tokio::spawn(raft.lead());
+            let http_addr = options.web_server.clone().unwrap();
+            let server = tokio::spawn(async move {
                 HttpServer::new(move || {
                     App::new()
-                        .app_data(web::Data::new(tx.clone()))
+                        .app_data(web::Data::new(mailbox.clone()))
                         .service(put)
                 })
-                .bind(options.web_server.unwrap())?
-                .run());
-            let (res1, res2) = tokio::try_join!(raft, server)?;
+                .bind(http_addr)
+                    .unwrap()
+                    .run()
+            });
+            let (res1, _res2) = tokio::try_join!(raft, server)?;
             res1?;
-            res2?;
         }
     }
 
     sys.await?;
     Ok(())
 }
-
-//use std::collections::HashMap;
-//use std::time::Duration;
-
-//use actix_web::{get, web, App, HttpServer, Responder};
-//use bincode::deserialize;
-//use crate::message::{Message, Proposal, RaftResponse};
-//use log::info;
-//use raft::eraftpb::ConfChange;
-//use raft_node::RaftNode;
-//use raft_server::RaftServer;
-//use raft_service::ConfigChange;
-//use raft_service::raft_service_client::RaftServiceClient;
-//use structopt::StructOpt;
-//use tokio::sync::{oneshot, mpsc};
-//use tokio::time::delay_for;
-//use tonic::Request;
-//use protobuf::Message as _;
-
-//#[derive(Debug, StructOpt)]
-//struct Options {
-    //#[structopt(long)]
-    //raft_addr: String,
-    //#[structopt(long)]
-    //peer_addr: Option<String>,
-    //#[structopt(long)]
-    //web_server: Option<String>,
-//}
-
-//#[get("/put/{id}/{name}")]
-//async fn put(sender: web::Data<mpsc::Sender<Message>>, path: web::Path<(u64, String)>) -> impl Responder {
-    //info!("received put request ({}, {})", path.0, path.1);
-    //let proposal = Proposal::Put { key: path.0, value: path.1.clone() };
-    //let (tx, rx) = oneshot::channel();
-    //let message = Message::Propose {
-        //seq: 100,
-        //proposal,
-        //chan: tx,
-    //};
-
-    //let _ = sender
-        //.as_ref()
-        //.clone()
-        //.send(message)
-        //.await;
-    //let _ = rx.await;
-    //format!("OK")
-//}
-
-//#[get("/remove/{id}")]
-//async fn remove(sender: web::Data<mpsc::Sender<Message>>, path: web::Path<u64>) -> impl Responder {
-    //let proposal = Proposal::Remove { key: path.into_inner() };
-    //let (tx, rx) = oneshot::channel();
-    //let message = Message::Propose {
-        //seq: 100,
-        //proposal,
-        //chan: tx,
-    //};
-
-    //let _ = sender
-        //.as_ref()
-        //.clone()
-        //.send(message)
-        //.await;
-    //let _ = rx.await;
-    //format!("OK")
-//}
-//#[tokio::main]
-//#[allow(irrefutable_let_patterns)]
-//async fn main() -> Result<(), Box<dyn std::error::Error>> {
-
-    //let (tx, rx) = mpsc::channel(100);
-
-    //// init the logger
-    //env_logger::init();
-
-    //let options = Options::from_args();
-    //// Select some defaults, then change what we need.
-
-    //// create rpc server and run it
-    //let raft_server = RaftServer::new(tx.clone(), options.raft_addr.clone());
-
-    //let store: HashMap<u64, String> = HashMap::new();
-
-    //let server_handle = tokio::spawn(raft_server.run());
-
-    //let local = tokio::task::LocalSet::new();
-    //let sys = actix_rt::System::run_in_tokio("server", &local);
-    //// if node is started without a peer addr, this is the first node of the cluster and it is
-    //// started as the leader.
-    //let mut node = match options.peer_addr {
-        //Some(host) => {
-            //// add peer to node's peers
-            //let mut host = host.to_string();
-            //loop {
-                //let mut client = RaftServiceClient::connect(host.clone()).await?;
-                //let mut conf_change = ConfChange::default();
-                //conf_change.set_context(format!("http://{}", options.raft_addr).into_bytes());
-                
-                //let conf_change_request = ConfigChange {
-                    //inner: conf_change.write_to_bytes().unwrap(),
-                //};
-
-                //let request = Request::new(conf_change_request);
-
-                //// get added peer and attempt to join it's cluster
-                //let response = client.change_config(request).await?.into_inner();
-                //let raft_response: RaftResponse  = deserialize(&response.inner).unwrap();
-                //match raft_response {
-                    //RaftResponse::JoinSuccess { assigned_id, peer_addrs } => {
-                        //info!("joined successfully, got id {}", assigned_id);
-                        //let mut node = RaftNode::new(rx, assigned_id);
-                        //for (id, addr) in peer_addrs {
-                            //let _ = node.add_peer(&addr, id).await;
-                        //}
-                        //break node;
-                    //}
-                    //RaftResponse::WrongLeader { leader_addr, .. } => {
-                        //host = leader_addr;
-                        //info!("Wrong leader, try again with leader at {}", host);
-                    //}
-                    //RaftResponse::Error => {
-                        //info!("there was an error joining the cluster");
-                        //delay_for(Duration::from_millis(1000)).await;
-                    //}
-                    //_ => unreachable!()
-                //}
-            //}
-        //}
-        //None => RaftNode::new(rx, 1),
-    //};
-
-    //// add current node to it's peers, needed to comunicate it to other nodes when they join
-    //let _ = node.add_peer(&format!("http://{}", options.raft_addr), node.id()).await.unwrap();
-
-    //let node_handle = tokio::spawn(node.run());
-
-    //// server for testing purpose
-    //if let Some(addr) = options.web_server {
-        //let http_handle = tokio::spawn(
-            //HttpServer::new(move || {
-                //App::new()
-                    //.app_data(web::Data::new(tx.clone()))
-                    //.service(put)
-                    //.service(remove)
-            //})
-            //.bind(addr)?
-            //.run());
-            //let _ = tokio::try_join!(server_handle, node_handle, http_handle)?;
-    //} else {
-            //let _ = tokio::try_join!(server_handle, node_handle)?;
-    //}
-
-    //sys.await?;
-
-    //Ok(())
-//}
--- src/raft.rs
+++ src/raft.rs
@@ -1,4 +1,4 @@
-use crate::message::Message;
+use crate::message::{Message, RaftResponse};
 use crate::raft_node::RaftNode;
 use crate::raft_server::RaftServer;
 use crate::raft_service::raft_service_client::RaftServiceClient;
@@ -9,14 +9,12 @@ use bincode::deserialize;
 use protobuf::Message as _;
 use raft::eraftpb::{ConfChange, ConfChangeType};
 use serde::{de::DeserializeOwned, Deserialize, Serialize};
-use std::sync::{Arc, RwLock};
-use tokio::sync::mpsc;
+use tokio::sync::{mpsc, oneshot};
 use tonic::Request;
 use bincode::serialize;
-use log::info;
 
-use std::collections::HashMap;
 use std::fmt;
+use std::sync::{Arc, RwLock};
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Leader {
@@ -31,11 +29,6 @@ pub trait Store: Send + Sync {
     fn apply(&mut self, message: Self::Message) -> Result<(), Self::Error>;
 }
 
-#[derive(Debug, Serialize, Deserialize)]
-pub enum MyMessage {
-    Insert { key: u64, value: String },
-}
-
 #[derive(Debug)]
 pub struct RaftError;
 
@@ -47,88 +40,104 @@ impl fmt::Display for RaftError {
 
 impl std::error::Error for RaftError {}
 
-impl Store for HashMap<u64, String> {
-    type Message = MyMessage;
-    type Error = RaftError;
 
-    fn apply(&mut self, message: Self::Message) -> Result<(), Self::Error> {
-        match message {
-            MyMessage::Insert { key, value } => {
-                info!("inserting: ({}, {})", key, value);
-                self.insert(key, value);
+#[derive(Clone)]
+pub struct Mailbox<M>(mpsc::Sender<Message<M>>) where M: Sync + Send;
+
+impl<M: Sync + Send> Mailbox<M> {
+    pub async fn send(&self, message: M) -> Result<(), RaftError> {
+        let (tx, rx) = oneshot::channel();
+        let proposal = Message::Propose { proposal: message, chan: tx };
+        let mut sender = self.0.clone();
+        match sender.send(proposal).await {
+            Ok(_) => {
+                match rx.await {
+                    Ok(RaftResponse::Ok) => Ok(()),
+                    _ => Err(RaftError),
+                }
             }
+            _ => Err(RaftError),
         }
-        Ok(())
     }
 }
 
-pub async fn run<S: Store + 'static>(
-    raft_addr: String,
-    store: Arc<RwLock<S>>,
+pub struct Raft<S: Store + 'static> {
+    store: S,
     tx: mpsc::Sender<Message<S::Message>>,
     rx: mpsc::Receiver<Message<S::Message>>,
-) -> Result<()> {
-    // TODO: arbitrary buffer length, may need to tune this?
-    let node = RaftNode::new_leader(rx, tx.clone(), store);
-    let server = RaftServer::new(tx, raft_addr);
-    let server_handle = tokio::spawn(server.run());
-    let node_handle = tokio::spawn(node.run());
-    tokio::try_join!(server_handle, node_handle)?;
-
-    Ok(())
+    addr: String,
 }
 
-pub async fn join<S: Store + 'static>(
-    raft_addr: &str,
-    peer_addr: &str,
-    store: Arc<RwLock<S>>,
-    tx: mpsc::Sender<Message<S::Message>>,
-    rx: mpsc::Receiver<Message<S::Message>>,
-) -> Result<()> {
-    // 1. try to discover the leader and obtain an id from it.
-    let mut leader_addr = peer_addr.to_string();
-    let (leader_id, node_id): (u64, u64) = loop {
-        let mut client = RaftServiceClient::connect(format!("http://{}", leader_addr)).await?;
-        let response = client
-            .request_id(Request::new(Empty::default()))
-            .await?
-            .into_inner();
-        match response.code() {
-            ResultCode::WrongLeader => {
-                let leader: Leader = deserialize(&response.data)?;
-                leader_addr = leader.addr;
-                continue;
-            }
-            ResultCode::Ok => break deserialize(&response.data)?,
-            ResultCode::Error => {
-                return Err(anyhow!(
-                    "join error: {}",
-                    deserialize::<String>(&response.data)?
-                ))
-            }
-        }
-    };
+impl<S: Store + 'static> Raft<S> {
+    pub fn new(addr: String, store: S) -> Self {
+        let (tx, rx) = mpsc::channel(100);
+        Self { store, tx, rx, addr }
+    }
 
-    // 2. run server and node to prepare for joining
-    let mut node = RaftNode::new_follower(rx, tx.clone(), node_id, store);
-    node.add_peer(&leader_addr, leader_id).await?;
-    let mut client = node.peer_mut(leader_id).unwrap().clone();
-    let server = RaftServer::new(tx, raft_addr);
-    let server_handle = tokio::spawn(server.run());
-    let node_handle = tokio::spawn(node.run());
+    pub fn mailbox(&self) -> Mailbox<S::Message> {
+        Mailbox(self.tx.clone())
+    }
 
-    // 3. Join the cluster
-    // TODO: handle leader change
-    let mut change = ConfChange::default();
-    change.set_node_id(node_id);
-    change.set_change_type(ConfChangeType::AddNode);
-    change.set_context(serialize(&raft_addr)?);
-    client
-        .change_config(Request::new(ConfigChange {
-            inner: change.write_to_bytes()?,
-        }))
+    pub async fn lead(self) -> Result<()> {
+        let addr = self.addr.clone();
+        let store = Arc::new(RwLock::new(self.store));
+        let node = RaftNode::new_leader(self.rx, self.tx.clone(), store);
+        let server = RaftServer::new(self.tx, addr);
+        let server_handle = tokio::spawn(server.run());
+        let node_handle = tokio::spawn(node.run());
+        tokio::try_join!(server_handle, node_handle)?;
+
+        Ok(())
+    }
+
+    pub async fn join(self, peer_addr: &str) -> Result<()> {
+        // 1. try to discover the leader and obtain an id from it.
+        let mut leader_addr = peer_addr.to_string();
+        let (leader_id, node_id): (u64, u64) = loop {
+            let mut client = RaftServiceClient::connect(format!("http://{}", leader_addr)).await?;
+            let response = client
+                .request_id(Request::new(Empty::default()))
+                .await?
+                .into_inner();
+            match response.code() {
+                ResultCode::WrongLeader => {
+                    let leader: Leader = deserialize(&response.data)?;
+                    leader_addr = leader.addr;
+                    continue;
+                }
+                ResultCode::Ok => break deserialize(&response.data)?,
+                ResultCode::Error => {
+                    return Err(anyhow!(
+                            "join error: {}",
+                            deserialize::<String>(&response.data)?
+                    ))
+                }
+            }
+        };
+
+        // 2. run server and node to prepare for joining
+        let addr = self.addr.clone();
+        let store = Arc::new(RwLock::new(self.store));
+        let mut node = RaftNode::new_follower(self.rx, self.tx.clone(), node_id, store);
+        node.add_peer(&leader_addr, leader_id).await?;
+        let mut client = node.peer_mut(leader_id).unwrap().clone();
+        let server = RaftServer::new(self.tx, addr);
+        let server_handle = tokio::spawn(server.run());
+        let node_handle = tokio::spawn(node.run());
+
+        // 3. Join the cluster
+        // TODO: handle wrong leader
+        let mut change = ConfChange::default();
+        change.set_node_id(node_id);
+        change.set_change_type(ConfChangeType::AddNode);
+        change.set_context(serialize(&self.addr)?);
+        client
+            .change_config(Request::new(ConfigChange {
+                inner: change.write_to_bytes()?,
+            }))
         .await?;
-    tokio::try_join!(server_handle, node_handle)?;
+        tokio::try_join!(server_handle, node_handle)?;
 
-    Ok(())
+        Ok(())
+    }
 }
